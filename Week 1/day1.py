65ad317df6e33db9e0c6dfad9ee68c3189e354f8
ItwenzyI <kischoenhaar@gmail.com>
2025-08-01 18:40:12 +0200

    Add Day1 and Day2

---
+++ Week 1/day1.py
@@ -0,0 +1,1 @@
+print("Not interesting. Already did in python-basics :)")
---
+++ Week 1/day2.py
@@ -0,0 +1,26 @@
+# Day 2: 30 Days of python programming
+
+first_name = "Kilian"
+last_name = "Zoldyck"
+full_name = first_name + " " + last_name
+a, x = 10, 12
+print(a)
+print(x)
+
+# Level 2
+print(type(x))
+print(type(first_name))
+print(len(first_name))
+print(len(first_name) > len(last_name))
+
+num_one = 5
+num_two = 4
+total_num = num_one + num_two
+tolal_sub_num = num_two - num_one
+total_mul_num = num_one * num_two
+total_div_num = num_one / num_two
+total_mod_num = num_one % num_two
+exp = num_one ** num_two
+total_floor_num = num_one // num_two
+
+help("keywords")
