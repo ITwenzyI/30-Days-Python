ce1ac51bba106e3871b427dff750987209c24530
Kilian <kischoenhaar@gmail.com>
2025-08-05 12:28:58 +0200

    Add Day17

---
+++ Week 3/day17.py
@@ -0,0 +1,12 @@
+# Day 17 - Exceptions
+# try:
+#     code in this block if things go well
+# except:
+#     code in this block run if things go wrong
+
+#1
+names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
+*nordic_countries, es, ru = names
+print(nordic_countries)
+print(es)
+print(ru)
