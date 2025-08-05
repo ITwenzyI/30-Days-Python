147096497d2ed373f4c6b08f6e0558543d96e8f3
Kilian <kischoenhaar@gmail.com>
2025-08-05 11:47:27 +0200

    Add Day15

---
+++ Week 3/day15.py
@@ -0,0 +1,37 @@
+# Day 15 - Type Errors
+
+# Syntax Error
+# print "Hello"
+
+# NameError
+# print(age)
+
+# IndexError
+# numbers = [1, 2, 3, 4, 5]
+# numbers[5]
+
+# ModuleNotFoundError
+# import maths
+
+# AttributeError
+# import math
+# math.PI
+
+# KeyError
+# users = {'name':'Kilian', 'age':99, 'country':'Germany'}
+# print(users['name'])
+# print(users['county'])
+
+# TypeError
+# print(4 + "3")
+
+# ImportError
+# from math import power ## Math only contains pow :)
+
+# ValueError
+# print(int("12a"))
+
+# ZeroDivisionError
+# print(8/0)
+
+
