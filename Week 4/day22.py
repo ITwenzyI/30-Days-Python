1adbb7ef543355f0de2b2f5924bf3b57db1b52fc
Kilian <kischoenhaar@gmail.com>
2025-08-08 10:52:40 +0200

    Add Day22

---
+++ Week 4/day22.py
@@ -0,0 +1,16 @@
+import json
+
+import requests
+from bs4 import BeautifulSoup
+url = 'http://www.bu.edu/president/boston-university-facts-stats/'
+
+response = requests.get(url)
+content = response.content # we get all the content from the website
+soup = BeautifulSoup(content, 'html.parser') # beautiful soup will give a chance to parse
+print(soup.title) # <title>UCI Machine Learning Repository: Data Sets</title>
+print(soup.title.get_text()) # UCI Machine Learning Repository: Data Sets
+print(soup.body) # gives the whole page on the website
+print(response.status_code)
+
+with open("content.json", "w", encoding="utf-8") as f:
+    f.write(soup.prettify())
